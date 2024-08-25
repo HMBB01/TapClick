9e63df0bb7cb72ea28f5c41b08d5eed3837e9521
lingh <2281442260@qq.com>
2024-08-25 23:59:17 +0800

    V3.31 优化代码

--- app/src/main/java/com/lgh/tapclick/myclass/MyApplication.java
+++ app/src/main/java/com/lgh/tapclick/myclass/MyApplication.java
@@ -71,7 +71,13 @@ public class MyApplication extends Application {
                     database.execSQL("ALTER TABLE 'Widget' ADD COLUMN 'condition' INTEGER NOT NULL DEFAULT 0");
                 }
             };
-            dataDao = Room.databaseBuilder(base, MyDatabase.class, "applicationData.db").addMigrations(migration_1_2, migration_2_3, migration_3_4, migration_4_5, migration_5_6, migration_6_7).allowMainThreadQueries().build().dataDao();
+            Migration migration_7_8 = new Migration(7, 8) {
+                @Override
+                public void migrate(@NonNull SupportSQLiteDatabase database) {
+                    database.execSQL("ALTER TABLE 'AppDescribe' DROP COLUMN 'onOff' ");
+                }
+            };
+            dataDao = Room.databaseBuilder(base, MyDatabase.class, "applicationData.db").addMigrations(migration_1_2, migration_2_3, migration_3_4, migration_4_5, migration_5_6, migration_6_7, migration_7_8).allowMainThreadQueries().build().dataDao();
         }
 
         if (myAppConfig == null) {
